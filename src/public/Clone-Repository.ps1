66bc747b47679cae7cd54cca739a86577fab8d5c
Norman Dankert <norman.dankert@outlook.com>
2019-07-11 14:38:52 +0200

    add parameter validation

--- src/public/Clone-Repository.ps1
+++ src/public/Clone-Repository.ps1
@@ -4,71 +4,86 @@ Function Clone-Repository {
 		[ValidateNotNullOrEmpty()]
 		[string] $Url,
 
-		[switch] $Force
-	)
-
-	#------------------------------------------------------------------------------
-	# Configuration of available providers
-	#------------------------------------------------------------------------------
-	$GitProviders = @{
-		"Azure"     = {
-			if ($args[0] -Match "https://(?:\w+@)?dev.azure.com/(?<Organization>\w+)/(?<Project>\w+)/_git/(?<Repository>[\w-_]+)") {
-				return [io.path]::Combine($Matches.Organization, $Matches.Project, $Matches.Repository)
+		[parameter(Mandatory = $false)]
+		[ValidateScript( {
+				If (-Not ($_ | Test-Path)) {
+				throw "Target path does not exist."
 			}
+			If (-Not ($_ | Test-Path -PathType Container)) {
+			throw "Target path must be a folder."
 		}
+		return $true;
+	})]
+[string] $ProjectsBaseDirectory,
 
-		"GitHub"    = {
-			if ($args[0] -Match "https://github\.com/(?<UserOrOrganization>\w+)/(?<Repository>[\w-_]+)\.git") {
-				return [io.path]::Combine($Matches.UserOrOrganization, $Matches.Repository)
-			}
-		}
+[switch] $Force
+)
 
-		"Bitbucket" = {
-			if ($args[0] -Match "https://(?:\w+@)?bitbucket.org/(?<User>\w+)/(?<Repository>[\w-_]+)\.git") {
-				return [io.path]::Combine($Matches.User, $Matches.Repository)
-			}
-
+#------------------------------------------------------------------------------
+# Configuration of available providers
+#------------------------------------------------------------------------------
+$GitProviders = @{
+	"Azure"     = {
+		if ($args[0] -Match "https://(?:\w+@)?dev.azure.com/(?<Organization>\w+)/(?<Project>\w+)/_git/(?<Repository>[\w-_]+)") {
+			return [io.path]::Combine($Matches.Organization, $Matches.Project, $Matches.Repository)
 		}
 	}
 
-
-	#------------------------------------------------------------------------------
-	# Find the right provider and clone the repository
-	#------------------------------------------------------------------------------
-	$Match = $GitProviders.GetEnumerator() |
-		Select-Object @{n = "Provider"; e = { $_.Key } }, @{n = "Path"; e = { $_.Value.invoke($Url) } } |
-		Where-Object { $_.Path -ne $null } |
-		Select-Object -First 1
-
-	if ($Match) {
-		Write-Host "Found match for provider: $($Match.Provider)"
-
-		if ($Global:ProjectsDir) {
-			$TargetDirectory = [io.path]::Combine($Global:ProjectsDir, $Match.Provider, $Match.Path)
+	"GitHub"    = {
+		if ($args[0] -Match "https://github\.com/(?<UserOrOrganization>\w+)/(?<Repository>[\w-_]+)\.git") {
+			return [io.path]::Combine($Matches.UserOrOrganization, $Matches.Repository)
 		}
-		else {
-			Write-Error "No projects directory configured. Aborting."
+	}
+
+	"Bitbucket" = {
+		if ($args[0] -Match "https://(?:\w+@)?bitbucket.org/(?<User>\w+)/(?<Repository>[\w-_]+)\.git") {
+			return [io.path]::Combine($Matches.User, $Matches.Repository)
 		}
 
-		if (Test-Path -PathType Container $TargetDirectory) {
-			if ($Force) {
-				Write-Host "Removing existing target directory."
-				Remove-Item -Recurse -Force $TargetDirectory
-			}
-			else {
-				Write-Error "Target directory already exists. Use -Force to overwrite. Aborting."
-			}
-		}
+	}
+}
 
-		git.exe clone $Url $TargetDirectory
 
-		if ($LASTEXITCODE -eq 0) {
-			Set-Location $TargetDirectory
-		}
+#------------------------------------------------------------------------------
+# Find the right provider and clone the repository
+#------------------------------------------------------------------------------
+$Match = $GitProviders.GetEnumerator() |
+	Select-Object @{n = "Provider"; e = { $_.Key } }, @{n = "Path"; e = { $_.Value.invoke($Url) } } |
+	Where-Object { $_.Path -ne $null } |
+	Select-Object -First 1
+
+if ($Match) {
+	Write-Host "Found match for provider: $($Match.Provider)"
+
+	if ($ProjectsBaseDirectory) {
+		$TargetDirectory = [io.path]::Combine($ProjectsBaseDirectory, $Match.Provider, $Match.Path)
+	}
+	elseif ($Global:ProjectsDir) {
+		$TargetDirectory = [io.path]::Combine($Global:ProjectsDir, $Match.Provider, $Match.Path)
 	}
 	else {
-		Write-Error "No match found for repository url: $Url"
+		Write-Error "Neither parameter ProjectsBaseDirectory nor global ProjectsDir are set. Aborting."
 	}
+
+	if (Test-Path -PathType Container $TargetDirectory) {
+		if ($Force) {
+			Write-Host "Removing existing target directory."
+			Remove-Item -Recurse -Force $TargetDirectory
+		}
+		else {
+			Write-Error "Target directory already exists. Use -Force to overwrite. Aborting."
+		}
+	}
+
+	git.exe clone $Url $TargetDirectory
+
+	if ($LASTEXITCODE -eq 0) {
+		Set-Location $TargetDirectory
+	}
+}
+else {
+	Write-Error "No match found for repository url: $Url"
+}
 }
 
 Export-ModuleMember -Function Clone-Repository
